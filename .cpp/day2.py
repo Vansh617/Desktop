a3c5c0d3095751ab6dbc171678f432d5efc0b50f
Vansh617 <v33990093@gmail.com>
2025-07-02 10:10:56 +0530

    first commit

---
+++ .cpp/day2.py
@@ -0,0 +1,48 @@
+
+# Day1
+
+# # print("Hi");
+# # a = 3
+# # b = 6
+# # c = a + b
+# # print(c)
+# a=344444444444444444444444
+# print(type(a))
+# f=3j
+# print(type(f))
+# t= True;
+# print(type(t));
+# str="is a stirng"
+# print(str[3:11])
+# std="mynameisneeraj"
+# print(std[0:8:4])
+# print("ths","hsasi",sep="-")
+# print("thi ai my first page",end=".\n")
+# # a=input("enter your number1:");
+# # b=input("enter your number2:")
+# # c=int(a)+int(b)
+# # print(c)
+
+# # a=0;
+# # b=bool(a)
+# # print(b)
+# # 
+# principle=34
+# rate=0.05
+# time=2
+# simple_interest = (principle * rate * time) / 100
+# #  print("Simple Interest: {simple_interest}")
+# print("Simple Interest: ", simple_interest)
+
+
+# bill=1200
+# friends=bill/4
+# print("Each friend should pay: ", friends)
+
+
+
+a=10
+if(a==0):
+    print("odd number")
+else:
+    print("even number")
