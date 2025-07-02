4db16b4f59dab782e22b5795ed32f96f70048f73
Vansh617 <v33990093@gmail.com>
2025-07-02 11:14:37 +0530

    name

--- python/day3.py
+++ python/day3.py
@@ -1,5 +1,5 @@
 # Day 3
-Name="iamneeraj"
+Name="I am Neeraj"
 print(Name.islower())
 print(Name.swapcase())
 print(Name.isupper())
@@ -7,5 +7,10 @@ print(Name.title())
 print(len(Name))
 print(Name.isdigit())
 print(Name.isalpha())
+print(Name.startswith('I'))
+print(Name.endswith('j'))
 
-
+print(Name.strip()) #last and staring space cut kr deta hai
+s="---he--lo-----"
+print(s.rstrip('-'))
+print(s.lstrip('-'))
