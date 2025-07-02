d281b60245d23edb287680951ce2a8e917af5e1d
Vansh617 <v33990093@gmail.com>
2025-07-02 13:18:18 +0530

    save

--- python/day3.py
+++ python/day3.py
@@ -1,5 +1,6 @@
 # Day 3
 Name="I am Neeraj"
+print(id(Name))
 print(Name.islower())
 print(Name.swapcase())
 print(Name.isupper())
@@ -15,3 +16,34 @@ s="---he--lo-----"
 print(s.rstrip('-'))
 print(s.lstrip('-'))
 print(s.strip('-'))
+a=10
+b=10
+print(id(a))
+print(id(a+1))
+print(id(b))
+
+#oprators
+print(10+4)
+print("name"+"12")
+print(2**4)
+print("hello"*12)
+print(7/2)
+print(7//2)
+print(3%2)
+#relational operators
+print(5>2)
+print("hello"<"hi")
+print("ant">"Ant")
+#logical operators
+#and, or, not
+
+x=5
+y=10
+z=15
+print(x>y>z and y>x and z<y)
+print(x*3>z or 7+y<z**3 )
+print(not(x==y))
+print("me"not in"same")
+print("me" in"same")
+g="hellllllllllllllllooooooo"
+print(g.count('l'))
